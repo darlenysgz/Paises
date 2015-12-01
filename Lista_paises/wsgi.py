57c8cf82a3c7c10a50d6c390d42027ca8db6d797
Darlenys Gomez <darlenysgz@hotmail.com>
2015-12-01 19:16:15 -0400

    first commit

---
+++ Lista_paises/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Lista_paises project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Lista_paises.settings")
+
+application = get_wsgi_application()
