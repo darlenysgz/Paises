ccc946d21446cf45489ba875fffc5c52606f7301
Darlenys Gomez <darlenysgz@hotmail.com>
2015-12-01 20:49:36 -0400

    arreglado

--- myapp_list/views.py
+++ myapp_list/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Pais, Ciudad
+from .models import pais, ciudad
 from django.views import generic
 # Create your views here.
 
 class IndexView(generic.ListView):
-    model = Pais
+    model = pais
     template_name = 'myapp_list/index.html'
     context_object_name = 'Agregado_Reciente'
 
     def get_queryset(self):
-        return Pais.objects.all()
+        return pais.objects.all()
 
 class DetailView(generic.DetailView):
-    model = Pais
+    model = pais
     template_name = 'myapp_list/detail.html'
 
 
 class Resultados(generic.DetailView):
-    model = Pais
+    model = pais
     template_name = 'myapp_list/resultados.html'
 
 def ciudades(request, id_pais):
-    p = get_object_or_404(Pais, pk = id_pais)
+    p = get_object_or_404(pais, pk = id_pais)
     try:
         ciudad_seleccionada = p.ciudad_set.get(pk = request.POST['ciudad'])
-    except (KeyError, Ciudad.DoesNotExist):
+    except (KeyError, ciudad.DoesNotExist):
         return render(request, 'myapp_list/detail.html', {
             'pais': p,
             'error_message': "No has hecho ninguna seleccion.",
