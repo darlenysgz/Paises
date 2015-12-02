b8949826dfaabc9e8e3c971bcdc3254bb628d7b8
Darlenys Gomez <darlenysgz@hotmail.com>
2015-12-01 20:31:59 -0400

    app lista

--- myapp_list/admin.py
+++ myapp_list/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import pais, ciudad, lenguaje
 
 # Register your models here.
+
+class CiudadInline(admin.TabularInline):
+    model = ciudad
+    extra = 0
+
+class LenguajeInline(admin.TabularInline):
+    model = lenguaje
+    extra = 0
+
+
+class paisAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Informacion General',{'fields': ['Codigo','Nombre','Continente']}),
+    ]
+
+
+
+admin.site.register(pais,paisAdmin)
